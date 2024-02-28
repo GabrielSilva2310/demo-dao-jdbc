596d3ca5aecd0dd067f2b4bcfdd0c2e13acce9ad
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-02-28 18:15:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,15 +2,23 @@ package Application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Department obj=new Department(1, "Cars");
+		
 		Seller seller=new Seller(10, "Gabriel", "gabriel@gmail.com", new Date(), 5000.00, obj);
+		
+		SellerDao sd=DaoFactory.createdSellerDao();
+		
 		System.out.println(seller);
+		
 
 	}
 
