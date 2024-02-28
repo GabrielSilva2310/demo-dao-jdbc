e68e6c9218eac5c4a95de1f18499b38f9d522582
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-02-28 17:19:27 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,16 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj=new Department(1, "Cars");
-		System.out.println(obj);
+		Seller seller=new Seller(10, "Gabriel", "gabriel@gmail.com", new Date(), 5000.00, obj);
+		System.out.println(seller);
 
 	}
 
