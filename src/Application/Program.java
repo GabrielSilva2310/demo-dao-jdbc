42bd7e2bd00b9cb59f362a4908279fc6c38433f5
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-03-03 19:27:16 -0300

    update implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -35,8 +35,13 @@ public class Program {
 		Seller newSeller=new Seller(null,"Gabriel", "gabriel@gmai.com",new Date(),5000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-	
 		
+		
+		System.out.println("\n==== TEST 5: seller Update =====");
+		seller=sellerDao.FindById(1);
+		seller.setName("Jude Bellingham");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 		
 
 	}
